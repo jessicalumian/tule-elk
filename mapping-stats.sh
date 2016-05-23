1c21842fff602fee37f10ca74d45b02ea7abb8f8
jessicamizzi <jessica.mizzi@gmail.com>
2016-05-23 13:18:10 -0700

    Create mapping-stats.sh

---
+++ mapping-stats.sh
@@ -0,0 +1,27 @@
+#!/bin/bash --login
+#PBS -l walltime=8:00:00
+#PBS -l mem=200gb
+#PBS -m abe
+#PBS -N elk-mapping-stats
+#PBS -A ged
+
+cd ${PBS_O_WORKDIR}
+
+source hpcc.modules
+
+# If you want to view the alignment
+# fix input file samtools tview all-interleaved-elk-reads.sorted.bam final.contigs.fa
+
+touch ${PBS_O_WORKDIR}/mapping-stats.txt
+
+echo Number of reads that did not align: >> ${PBS_O_WORKDIR}/mapping-stats.txt
+# How many reads did not align
+fix input file samtools view -c -f 4 all-interleaved-elk-reads.sorted.bam >> ${PBS_O_WORKDIR}/mapping-stats.txt
+
+echo Number of reads that aligned: >> ${PBS_O_WORKDIR}/mapping-stats.txt
+# How many reads did align
+fix input file samtools view -c -F 4 all-interleaved-elk-reads.sorted.bam >> ${PBS_O_WORKDIR}/mapping-stats.txt
+
+cat ${PBS_NODEFILE}
+env | grep PBS
+qstat -f ${PBS_JOBID}
