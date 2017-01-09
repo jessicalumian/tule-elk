aac4e55030d9bef26601a1af950367d6e9f12b40
jessicamizzi <jessica.mizzi@gmail.com>
2017-01-09 10:02:06 -0800

    change order of sorting/indexing/rmdup

--- 1339-mapping-snp-calling.sh
+++ 1339-mapping-snp-calling.sh
@@ -19,10 +19,13 @@ source hpcc.modules
 # convert sam to bam
 #samtools import /mnt/research/ged/jessica/elk/working-scripts/contigs.fa.fai 1339-elk-reads.sam 1339-elk-reads.bam
 
+# sort the bam file
+#samtools sort 1339-elk-reads.bam 1339-elk-reads.sorted
+
 ## NEW SECTION - REMOVING CLONES ##
 
 # mark duplicates based on start and end position of reads
-java -jar $PICARD/MarkDuplicates.jar I=1339-elk-reads.bam O=1339_nodup.bam M=Metrics
+java -jar $PICARD/MarkDuplicates.jar I=1339-elk-reads.sorted.bam O=1339_nodup.bam M=Metrics
 
 # remove duplicates from paired end reads ( -S flag for unpaired)
 samtools rmdup 1339_nodup.bam > 1339-nodup.bam
@@ -30,17 +33,14 @@ samtools rmdup 1339_nodup.bam > 1339-nodup.bam
 # prints per-site coverage and saves to text file
 samtools depth 1339-nodup.bam > 1339-nodup.bam
 
+#index the sorted bam file
+samtools index 1339-nodup.bam
+
 ## end new section
 
-# sort the bam file
-#samtools sort 1339-nodup.bam 1339-nodup.bam.sorted
-
-#index the sorted bam file
-#samtools index 1339-nodup.sorted.bam
-
 ## SNP CALLING ##
-#freebayes -f /mnt/research/ged/jessica/elk/working-scripts/final.contigs.fa --ploidy 2 --min-coverage 10 --no-mnps --no-complex --min-alternate-count 2 1339-nodup.sorted.bam | vcffilter -f "QUAL > 20" > 1339-nodup-min2.vcf
 
+#freebayes -f /mnt/research/ged/jessica/elk/working-scripts/final.contigs.fa --ploidy 2 --min-coverage 10 --no-mnps --no-complex --min-alternate-count 2 1339-nodup.sorted.bam | vcffilter -f "QUAL > 20" > 1339-nodup-min2.vcf
 
 cat ${PBS_NODEFILE}
 env | grep PBS
